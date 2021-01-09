60d7891d08a3f17684c18ce86209b2e29abd110e
lsromao <lucas.souzaromao@gmai.com>
2021-01-09 14:42:48 +0100

    First commit

---
+++ docker/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# TODO Provide support for GPU
+NAME="cad-final-project"
+docker run -it --rm -d \
+    --name "$NAME" \
+    -p 4567:4567 \
+    -v /tmp/log:/root/.ros \
+    -v $(pwd):/host \
+    --workdir /host \
+    360lab/cad-final-project bash \
+    --gpus=all
+
+echo "Container '$NAME' is now running. You can develop inside it by attaching VSCode to it."
+
